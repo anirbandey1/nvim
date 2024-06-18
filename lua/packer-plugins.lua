884dcc48a1599e926d4658e5dac09c6a8af6929c
Anirban Dey <anirbandey@soufrabi.com>
2024-06-18 13:00:27 +0530

    format file lua packer-plugins

--- lua/packer-plugins.lua
+++ lua/packer-plugins.lua
@@ -5,80 +5,80 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-	-- Vim commentary
-	use { 'tpope/vim-commentary' }
+    -- Vim commentary
+    use { 'tpope/vim-commentary' }
 
-	-- Ale
-	-- use { 'dense-analysis/ale' }
+    -- Ale
+    -- use { 'dense-analysis/ale' }
 
-	-- Coc
-	-- use { 'neoclide/coc.nvim', branch = 'release' }
-
-
-	-- Lsp-zero
-	use {
-	'VonHeikemen/lsp-zero.nvim',
-	branch = 'v1.x',
-	requires = {
-	-- LSP Support
-	{'neovim/nvim-lspconfig'},             -- Required
-	{'williamboman/mason.nvim'},           -- Optional
-	{'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-	-- Autocompletion
-	{'hrsh7th/nvim-cmp'},         -- Required
-	{'hrsh7th/cmp-nvim-lsp'},     -- Required
-	{'hrsh7th/cmp-buffer'},       -- Optional
-	{'hrsh7th/cmp-path'},         -- Optional
-	{'saadparwaiz1/cmp_luasnip'}, -- Optional
-	{'hrsh7th/cmp-nvim-lua'},     -- Optional
-
-	-- Snippets
-	{'L3MON4D3/LuaSnip'},             -- Required
-	{'rafamadriz/friendly-snippets'}, -- Optional
-	}
-	}
+    -- Coc
+    -- use { 'neoclide/coc.nvim', branch = 'release' }
 
 
+    -- Lsp-zero
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },         -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+            { 'hrsh7th/cmp-buffer' },       -- Optional
+            { 'hrsh7th/cmp-path' },         -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },     -- Optional
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },             -- Required
+            { 'rafamadriz/friendly-snippets' }, -- Optional
+        }
+    }
 
 
-	-- Ultisnippets
-	-- use {'SirVer/ultisnips'}
-
-	-- NvimTree
-	-- use {'nvim-tree/nvim-tree.lua'}
-	use {
-		'nvim-tree/nvim-tree.lua',
-		requires = {
-			'nvim-tree/nvim-web-devicons', -- optional, for file icons
-		},
-		-- tag = 'nightly'           -- optional, updated every week. (see issue #1193)
-	}
-
-	-- NerdTree
-	-- use {'preservim/nerdtree'}
-
-	-- Vim-Airline
-	use { 'vim-airline/vim-airline' }
-	use { 'vim-airline/vim-airline-themes' }
-
-	-- Fugitive
-	use { 'tpope/vim-fugitive' }
-
-	-- Polyglot
-	-- use { 'sheerun/vim-polyglot' }
-
-	-- Gruvbox
-	-- use {'morhetz/gruvbox'}
-
-	-- Vim - colorschemes
-	use { 'flazz/vim-colorschemes' }
 
 
-	-- nvim-treesitter
+    -- Ultisnippets
+    -- use {'SirVer/ultisnips'}
+
+    -- NvimTree
+    -- use {'nvim-tree/nvim-tree.lua'}
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+        },
+        -- tag = 'nightly'           -- optional, updated every week. (see issue #1193)
+    }
+
+    -- NerdTree
+    -- use {'preservim/nerdtree'}
+
+    -- Vim-Airline
+    use { 'vim-airline/vim-airline' }
+    use { 'vim-airline/vim-airline-themes' }
+
+    -- Fugitive
+    use { 'tpope/vim-fugitive' }
+
+    -- Polyglot
+    -- use { 'sheerun/vim-polyglot' }
+
+    -- Gruvbox
+    -- use {'morhetz/gruvbox'}
+
+    -- Vim - colorschemes
+    use { 'flazz/vim-colorschemes' }
+
+
+    -- nvim-treesitter
     use {
         'nvim-treesitter/nvim-treesitter',
         run = function()
@@ -87,87 +87,85 @@ return require('packer').startup(function(use)
         end,
     }
 
-	-- Indent line
-	-- use { 'Yggdroot/indentLine' }
+    -- Indent line
+    -- use { 'Yggdroot/indentLine' }
 
-	-- indent-blankline
-	use "lukas-reineke/indent-blankline.nvim"
+    -- indent-blankline
+    use "lukas-reineke/indent-blankline.nvim"
 
-	-- Vim obsession
-	use { 'tpope/vim-obsession' }
+    -- Vim obsession
+    use { 'tpope/vim-obsession' }
 
-	-- Emmet abbreviation
-	-- use { 'mattn/emmet-vim' }
+    -- Emmet abbreviation
+    -- use { 'mattn/emmet-vim' }
 
-	-- Latex
-	-- use { 'lervag/vimtex' }
+    -- Latex
+    -- use { 'lervag/vimtex' }
 
-	-- Latex preview
-	-- use {'xuhdev/vim-latex-live-preview'}
-	-- doesn't provide linting
+    -- Latex preview
+    -- use {'xuhdev/vim-latex-live-preview'}
+    -- doesn't provide linting
 
-	-- Markdown preview
-	use({
-		"iamcco/markdown-preview.nvim",
-		run = "cd app && npm install",
-		setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
-		ft = { "markdown" },
-	})
+    -- Markdown preview
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = "cd app && npm install",
+        setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
+        ft = { "markdown" },
+    })
 
-	-- Fzf
-	-- use { 'junegunn/fzf', run = function() vim.fn['fzf#install']() end }
-	-- use 'junegunn/fzf.vim'
+    -- Fzf
+    -- use { 'junegunn/fzf', run = function() vim.fn['fzf#install']() end }
+    -- use 'junegunn/fzf.vim'
 
-	-- nvim-telescope
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		-- or                            , branch = '0.1.x',
-		requires = { { 'nvim-lua/plenary.nvim' } }
-	}
-
-
-	-- Vim bookmarks
-	-- use { 'MattesGroeger/vim-bookmarks' }
-
-	-- Folds
-	use { 'anuvyklack/pretty-fold.nvim',
-		config = function()
-			require('pretty-fold').setup()
-		end
-	}
+    -- nvim-telescope
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
 
-	-- Catppuccin
-	-- colorscheme works better with true colorsupport
-	use { "catppuccin/nvim", as = "catppuccin" }
+    -- Vim bookmarks
+    -- use { 'MattesGroeger/vim-bookmarks' }
 
-	-- Image Viewer using Ascii art
-	use {
-		'samodostal/image.nvim',
-		requires = {
-			'nvim-lua/plenary.nvim'
-		},
-	}
-
-	-- Marks plugin
-	-- use { "chentoast/marks.nvim" }
-	-- Unable to delete marks properly for some representation
-	-- Not a problem of the plugin
-
-	-- Undo tree written in vimscript
-	-- use {'mbbill/undotree'}
+    -- Folds
+    use { 'anuvyklack/pretty-fold.nvim',
+        config = function()
+            require('pretty-fold').setup()
+        end
+    }
 
 
-	-- Undo-Tree written in lua
-	use {
-		"jiaoshijie/undotree",
-		requires = {
-			"nvim-lua/plenary.nvim",
-		},
-	}
+    -- Catppuccin
+    -- colorscheme works better with true colorsupport
+    use { "catppuccin/nvim", as = "catppuccin" }
+
+    -- Image Viewer using Ascii art
+    use {
+        'samodostal/image.nvim',
+        requires = {
+            'nvim-lua/plenary.nvim'
+        },
+    }
+
+    -- Marks plugin
+    -- use { "chentoast/marks.nvim" }
+    -- Unable to delete marks properly for some representation
+    -- Not a problem of the plugin
+
+    -- Undo tree written in vimscript
+    -- use {'mbbill/undotree'}
+
+
+    -- Undo-Tree written in lua
+    use {
+        "jiaoshijie/undotree",
+        requires = {
+            "nvim-lua/plenary.nvim",
+        },
+    }
 
     -- Jupyter notebook
     -- use { 'dccsillag/magma-nvim', run = ':UpdateRemotePlugins' }
-
-
 end)
